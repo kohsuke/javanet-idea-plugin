423ccbf7393e8b79c371338c9e32463c4bd0eb82
Kohsuke Kawaguchi <kk@kohsuke.org>
2008-07-06 19:39:03 +0000

    making progress

--- src/org/jvnet/idea_plugin/issue_tracker/editor/EditorForm.java
+++ src/org/jvnet/idea_plugin/issue_tracker/editor/EditorForm.java
@@ -1,6 +1,9 @@
 package org.jvnet.idea_plugin.issue_tracker.editor;
 
+import com.intellij.ui.JScrollPane2;
+
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -12,16 +15,30 @@ public class EditorForm {
     private JPanel properties;
     private JPanel form;
 
+    private JScrollPane outerMost;
+
+    /**
+     * IntelliJ chokes when I try to place certain components,
+     * so this is a work around to manually create them.
+     */
     public void postInit() {
         panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
         for(int i=0; i<10; i++) {
             JLabel label = new JLabel("test1",JLabel.LEFT);
             panel.add(label);
-            panel.add(new JTextArea("long\ntext"));
+
+            JTextArea text = new JTextArea("long\ntext");
+            text.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+            panel.add(text);
         }
+
+        outerMost = new JScrollPane(form,
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        outerMost.setBorder(BorderFactory.createEmptyBorder());
     }
 
     public JComponent getForm() {
-        return form;
+        return outerMost;
     }
 }
